f8f119882a01f480cb590966b3a089056e785425
Sindon20 <ginessindonjhamaicamae@gmail.com>
2025-04-04 21:53:33 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,10 @@
+const fanSinger = ['Taylor Swift', 'Ed Sheeran', 'Bamboo'];
+console.log(fanSingers[0]);
+
+const fanNumbers = [10, 15, 40, 98];
+const musical = ['string', ['otherarray'], 123, true];
+
+console.log(musical[0]);
+console.log(musical[1]);
+console.log(musical[2]);
+console.log(musical[3]);
